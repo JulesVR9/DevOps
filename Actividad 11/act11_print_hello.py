c8c5c13ce4710f033a80ddcbe73a448803ea8fcf
JulesVR <vallejo.rodriguez.julia@gmail.com>
2025-04-05 21:45:48 -0600

    Create act11_print_hello.py

---
+++ Actividad 11/act11_print_hello.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  5 21:41:59 2025
+
+@author: julia
+"""
+
+# act11_print_hello.py
+name = "Julia"  
+print(f"Hello {name}, el Job se ha completado correctamente.")
